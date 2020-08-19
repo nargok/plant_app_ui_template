d794e058305f9d0e396ed0133aa32a4068394042
Ryoichi Gokan <nargok@gmail.com>
2020-08-19 09:16:59 +0900

    search text field

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -8,13 +8,50 @@ class Body extends StatelessWidget {
     return Column(
       children: [
         Container(
-          height: size.height * 0.2 - 27,
-          decoration: BoxDecoration(
-            color: kPrimaryColor,
-            borderRadius:  BorderRadius.only(
-              bottomLeft: Radius.circular(36),
-              bottomRight: Radius.circular(36),
-            )
+          height: size.height * 0.2,
+          child: Stack(
+            children: [
+              Container(
+                height: size.height * 0.2 - 27,
+                decoration: BoxDecoration(
+                    color: kPrimaryColor,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(36),
+                      bottomRight: Radius.circular(36),
+                    )),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.23),
+                      )
+                    ],
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      hintStyle: TextStyle(
+                        color: kPrimaryColor.withOpacity(0.5),
+                      ),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ],
