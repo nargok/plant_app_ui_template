99c95bbf96cfe0404df7be6311888ebef23ef6f7
Ryoichi Gokan <nargok@gmail.com>
2020-08-20 10:44:15 +0900

    title with more button

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plant_ui_template/screens/home/components/header_with_searchbox.dart';
+import 'package:plant_ui_template/screens/home/components/title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -9,9 +10,12 @@ class Body extends StatelessWidget {
       child: Column(
         children: [
           HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(
+            title: 'More',
+            press: () {},
+          ),
         ],
       ),
     );
   }
 }
-
